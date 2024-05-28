e8e1817bfa586a8b7cd46596d59cb8049a7b89d5
quannguyen <tonyminhquan002@gmail.com>
2024-05-28 00:12:00 -0700

    my hw week 7B :>

---
+++ Week 7/compute.py
@@ -0,0 +1,66 @@
+
+import math
+
+# I can use the calculator to find the expected my output :))
+
+#1. abs(1.6 + -4)
+# Expected output: 2.4
+print("abs(1.6 + -4):", abs(1.6 + -4))
+
+#2. math.pow(6, 2)
+# Expected output: 36.0
+print("math.pow(6, 2):", math.pow(6, 2))
+
+#3. math.pow(5 // 2, 6)
+# Expected output: 64.0
+print("math.pow(5 // 2, 6):", math.pow(5 // 2, 6))
+
+#4. math.ceil(9.1)
+# Expected output: 10
+print("math.ceil(9.1):", math.ceil(9.1))
+
+#5. math.ceil(115.8)
+# Expected output: 116
+print("math.ceil(115.8):", math.ceil(115.8))
+
+#6. max(7, 4)
+# Expected output: 7
+print("max(7, 4):", max(7, 4))
+
+#7. min(8, 3 + 2)
+# Expected output: 5
+print("min(8, 3 + 2):", min(8, 3 + 2))
+
+#8. min(-2, -5)
+# Expected output: -5
+print("min(-2, -5):", min(-2, -5))
+
+#9. math.sqrt(49)
+# Expected output: 7.0
+print("math.sqrt(49):", math.sqrt(49))
+
+#10. math.sqrt(76 + 45)
+# Expected output: 11.0
+print("math.sqrt(76 + 45):", math.sqrt(76 + 45))
+
+#11. 100 + math.log10(100)
+# Expected output: 102.0
+print("100 + math.log10(100):", 100 + math.log10(100))
+
+#12. 13 + abs(-7) - math.pow(2, 3) + 5
+# Expected output: 7.0
+print("13 + abs(-7) - math.pow(2, 3) + 5:", 13 + abs(-7) - math.pow(2, 3) + 5)
+
+#13. math.sqrt(16) * max(abs(-5) , abs(-3))
+# Expected output: 20.
+print("math.sqrt(16) * max(abs(-5), abs(-3)):", math.sqrt(16) * max(abs(-5), abs(-3)))
+
+#14. 7 - 2 + math.log10(1000) + math.log(math.pow(math.e, 5))
+# Expected output: 12.0
+print("7 - 2 + math.log10(1000) + math.log(math.pow(math.e, 5)):", 7 - 2 + math.log10(1000) + math.log(math.pow(math.e, 5)))
+
+#15. max(18 - 5, math.ceil(4.6 * 3))
+# Expected output: 14
+print("max(18 - 5, math.ceil(4.6 * 3)):", max(18 - 5, math.ceil(4.6 * 3)))
+
+
